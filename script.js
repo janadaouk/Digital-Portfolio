17614d19829323c2271b672669c6c3f9daddb337
jana-daouk <jana.daouk@student.allenisd.org>
2022-09-22 23:37:44 -0500

    edited and made everything better

--- script.js
+++ script.js
@@ -1,22 +1,26 @@
 var factList=["You are amazing", "You are going to have a great day!", "You will do awesome!", "You look incredible!", "HAHA you're funny", "Your hair looks goregous today!", "Your fit is fire!"];
 
-console.log("hiiiiiiiiiiiiiii");
+//console.log("hiiiiiiiiiiiiiii");
 var myBtn= document.getElementById("factButton");
 
-var check = 0;
-var fact = document.getElementById("fact");
-
-function generateFact() {
+var check= 0;
+//var fact= document.getElementById("fact");
+console.log(check);
+    
+    function generateFact() {
 
   //  document.getElementById("fact").innerHTML = factList[factCount];
     var random = Math.floor(Math.random()*factList.length);
-    //console.log(check, random);
-    while (check == random) {
+    console.log(check, random);
+ 
+	while (check == random) {
 	var random = Math.floor(Math.random()*factList.length);
     }
-   // console.log(check, random);
-    fact.innerHTML = factList[random];
-    check = random;
+	// console.log(check, random);
+	//    console.log(factList[0]);
+	//  console.log(factList[random]);
+	document.getElementById("fact").innerHTML = factList[random];
+	check = random;
  //	console.log(factList[factCount]);
 }
 
